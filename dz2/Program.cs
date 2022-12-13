acce0bf4a1fa704634317f86aaff45f4562ad656
SvetlanaMedvedeva73 <sweta.medvedev16@yandex.ru>
2022-12-13 10:56:51 +0300

    dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+int[] array = new int[7];
+for (int i = 0; i < array.Length; i++)
+     array[i] = new Random().Next(7);
+
+int n = 0;
+
+for (int i = 1; i < array.Length; i += 2)
+     n += array[i];
+
+for (int i = 0; i < array.Length; i++)
+     Console.Write($"{array[i]} ");
+Console.Write($"-> {n}");
